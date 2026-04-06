f17154fc802f8b928ef834324ba394c3f1756f5c
tennisskip-bets <tennisskip@gmail.com>
2026-04-05 21:03:11 -0400

    Update mlbData.js

--- pages/api/mlbData.js
+++ pages/api/mlbData.js
@@ -1,6 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    status: "success",
-    message: "MLB API is working"
-  });
+  res.status(200).json({ message: "MLB Data API is working!" });
 }
